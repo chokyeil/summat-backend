8828e958e2c0cf4d7cc6f854795fa26e357287bc
chokyeil <meme951023@gmail.com>
2025-12-10 14:58:08 +0900

    "feat: PlacesDetailResDto add"

---
+++ src/main/java/com/summat/summat/places/dto/PlacesDetailResDto.java
@@ -0,0 +1,26 @@
+package com.summat.summat.places.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PlacesDetailResDto {
+    private String placeName;
+
+    private String placeDetailAddress;
+
+    private String placeDescription;
+
+    private String placeImageUrl;
+
+    private String oneLineDesc;
+
+    private String placeType;
+
+    private String placeRegion;
+
+    private long likeCount;
+
+    private long viewCount;
+}
