ce97e42ba79f3989e23a2ce95eb1b55b750b8549
chokyeil <meme951023@gmail.com>
2025-12-11 15:06:11 +0900

    "feat: viewCount update query add"

--- src/main/java/com/summat/summat/places/repository/PlacesRepository.java
+++ src/main/java/com/summat/summat/places/repository/PlacesRepository.java
@@ -2,8 +2,15 @@ package com.summat.summat.places.repository;
 
 import com.summat.summat.places.entity.Places;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PlacesRepository extends JpaRepository<Places, Long> {
+
+    @Modifying
+    @Query("UPDATE Places p SET p.viewCount = p.viewCount + 1 WHERE p.id = :placeId")
+    int increaseViews(@Param("placeId") Long placeId);
 }
