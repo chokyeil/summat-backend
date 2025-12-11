cc67fc1e27aefe97ebbedb90b96abc3766a39a82
chokyeil <meme951023@gmail.com>
2025-12-11 15:05:01 +0900

    "feat: viewCount permitAll add"

--- src/main/java/com/summat/summat/config/security/SecurityConfig.java
+++ src/main/java/com/summat/summat/config/security/SecurityConfig.java
@@ -45,6 +45,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/summatUsers/signup").permitAll()
                         .requestMatchers(HttpMethod.GET, "/places/list").permitAll()
                         .requestMatchers(HttpMethod.GET, "/places/detail/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/places/view/**").permitAll()
                         // 정적 리소스 등도 필요하면 여기 permitAll
                         // .requestMatchers("/", "/index.html", "/static/**").permitAll()
                         // 나머지는 인증 필요
