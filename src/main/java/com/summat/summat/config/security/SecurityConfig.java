0f59df1ef6460fcc2d01031cb342f6a2c97aca1f
chokyeil <meme951023@gmail.com>
2025-12-22 16:33:07 +0900

    "feat: place like add"

--- src/main/java/com/summat/summat/config/security/SecurityConfig.java
+++ src/main/java/com/summat/summat/config/security/SecurityConfig.java
@@ -46,6 +46,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/places/list").permitAll()
                         .requestMatchers(HttpMethod.GET, "/places/detail/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/places/view/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/places/like/**").permitAll()
                         // 정적 리소스 등도 필요하면 여기 permitAll
                         // .requestMatchers("/", "/index.html", "/static/**").permitAll()
                         // 나머지는 인증 필요
