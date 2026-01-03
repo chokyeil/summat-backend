464946c9808e31e9913976e7f34ed283feea5dfa
chokyeil <meme951023@gmail.com>
2026-01-03 16:23:31 +0900

    "feat: comment add & lot address, road address modify"

--- src/main/java/com/summat/summat/places/dto/places/PlacesDetailResDto.java
+++ src/main/java/com/summat/summat/places/dto/places/PlacesDetailResDto.java
@@ -6,21 +6,29 @@ import lombok.Setter;
 @Getter
 @Setter
 public class PlacesDetailResDto {
+
     private String placeName;
 
-    private String placeDetailAddress;
+    // 지번 주소 (선택 or 필수는 정책에 따라)
+    private String placeLotAddress;
 
-    private String placeDescription;
+    // 도로명 주소
+    private String placeRoadAddress;
 
-    private String placeImageUrl;
-
+    // 한 줄 설명
     private String oneLineDesc;
 
+    // 상세 설명
+    private String placeDescription;
+
+    // 업종 (카페/식당/빵집 등)
     private String placeType;
 
+    // 지역 (서울/부산 등)
     private String placeRegion;
 
     private long likeCount;
 
     private long viewCount;
+
 }
