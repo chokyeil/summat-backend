e85c49988c656ddb9f59745c23dd93c04d05116a
chokyeil <meme951023@gmail.com>
2026-01-03 16:26:09 +0900

    "feat: nick name check uri add"

--- src/main/java/com/summat/summat/users/controller/UsersController.java
+++ src/main/java/com/summat/summat/users/controller/UsersController.java
@@ -4,13 +4,11 @@ import com.summat.summat.users.CustomUserDetails;
 import com.summat.summat.users.dto.users.PasswordCheckReqDto;
 import com.summat.summat.users.dto.users.UsersReqDto;
 import com.summat.summat.users.service.UsersService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
@@ -22,7 +20,7 @@ public class UsersController {
     private final UsersService usersService;
 
     @PostMapping("/signup")
-    public HashMap<String, Object> signUp(@RequestBody UsersReqDto usersReqDto) {
+    public HashMap<String, Object> signUp(@RequestBody @Valid UsersReqDto usersReqDto) {
         log.info("UsersController signUp userId = " + usersReqDto.getUserId());
         log.info("UsersController signUp userPw = " + usersReqDto.getUserPw());
         log.info("UsersController signUp userNickName = " + usersReqDto.getUserNickName());
@@ -31,8 +29,8 @@ public class UsersController {
         log.info("UsersController signUp saveResult = " + saveResult);
         HashMap<String, Object> signUpResponse = new HashMap<>();
 
-        signUpResponse.put("status", saveResult == true ? 200 : 500);
-        signUpResponse.put("messagw", saveResult == true ? "회원가입 완료 했습니다." : "회원가입 실패 했습니다.");
+        signUpResponse.put("status", saveResult ? 200 : 500);
+        signUpResponse.put("messagw", saveResult ? "회원가입 완료 했습니다." : "회원가입 실패 했습니다.");
 
         return signUpResponse;
     }
@@ -47,6 +45,19 @@ public class UsersController {
         result.put("status", isCurrentPassword ? 200 : 500);
         result.put("message", isCurrentPassword ? "true current password" : "false current password");
         result.put("data", isCurrentPassword);
+
+        return result;
+    }
+
+    @GetMapping("/nick-name-check/{nickname}")
+    public HashMap<String, Object> checkCurrentNickName(@PathVariable String checkNickName) {
+        Boolean isNickName = usersService.checkCurrentNickName(checkNickName);
+
+        HashMap<String, Object> result = new HashMap<>();
+
+        result.put("status", isNickName ? 200 : 500);
+        result.put("message", isNickName ? "found nick name" : "not found nick name");
+        result.put("data", isNickName);
 
         return result;
     }
