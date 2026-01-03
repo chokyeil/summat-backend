12a3004ab74717ccf8fa84fc0a6f66a0dda7979c
chokyeil <meme951023@gmail.com>
2026-01-03 16:25:10 +0900

    "feat: permit password check, nick name check add"

--- src/main/java/com/summat/summat/config/security/SecurityConfig.java
+++ src/main/java/com/summat/summat/config/security/SecurityConfig.java
@@ -43,11 +43,12 @@ public class SecurityConfig {
                         // 인증 없이 접근 허용
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/summatUsers/signup").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/summatUsers/pw-check").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/summatUsers/nick-name-check/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/places/list").permitAll()
                         .requestMatchers(HttpMethod.GET, "/places/detail/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/places/view/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/places/like/**").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/mypage/**").permitAll()
                         // 정적 리소스 등도 필요하면 여기 permitAll
                         // .requestMatchers("/", "/index.html", "/static/**").permitAll()
                         // 나머지는 인증 필요
