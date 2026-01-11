b454fe838cf93be1b0f6eb19080a225adf3d1ec3
chokyeil <meme951023@gmail.com>
2026-01-11 22:13:01 +0900

    "feat: return type int -> boolean modified"

--- src/main/java/com/summat/summat/places/repository/PlacesRepository.java
+++ src/main/java/com/summat/summat/places/repository/PlacesRepository.java
@@ -1,16 +1,24 @@
 package com.summat.summat.places.repository;
 
 import com.summat.summat.places.entity.Places;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PlacesRepository extends JpaRepository<Places, Long> {
 
     @Modifying
     @Query("UPDATE Places p SET p.viewCount = p.viewCount + 1 WHERE p.id = :placeId")
-    int increaseViews(@Param("placeId") Long placeId);
+    boolean increaseViews(@Param("placeId") Long placeId);
+
+    Page<Places> findAll(Pageable pageable);
+
 }
