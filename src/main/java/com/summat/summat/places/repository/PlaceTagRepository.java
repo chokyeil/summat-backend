6e738f7fe389aa7cb1c0c86954a2beb73ae65df5
chokyeil <meme951023@gmail.com>
2026-01-11 22:13:31 +0900

    "feat: jpql query add"

--- src/main/java/com/summat/summat/places/repository/PlaceTagRepository.java
+++ src/main/java/com/summat/summat/places/repository/PlaceTagRepository.java
@@ -3,11 +3,15 @@ package com.summat.summat.places.repository;
 import com.summat.summat.enums.PlaceTagType;
 import com.summat.summat.places.entity.PlaceTag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
 @Repository
 public interface PlaceTagRepository extends JpaRepository<PlaceTag, Long> {
-    List<PlaceTagType> findByPlaceId(Long placeId);
+
+    @Query("SELECT pt.tagType FROM PlaceTag pt WHERE pt.place.id = :placeId")
+    List<PlaceTagType> findTagTypesByPlaceId(@Param("placeId") Long placeId);
 }
