b354b99fc942c6f34a43237448c1d715399628bf
chokyeil <meme951023@gmail.com>
2026-01-13 22:35:02 +0900

    "feat: list -> page object modified"

--- src/main/java/com/summat/summat/places/repository/PlacesRepository.java
+++ src/main/java/com/summat/summat/places/repository/PlacesRepository.java
@@ -20,6 +20,22 @@ public interface PlacesRepository extends JpaRepository<Places, Long> {
     @Query("UPDATE Places p SET p.viewCount = p.viewCount + 1 WHERE p.id = :placeId")
     boolean increaseViews(@Param("placeId") Long placeId);
 
+//    @Query("""
+//            SELECT
+//            	p.places_id,
+//            	p.place_name,
+//            	p.place_image_url,
+//            	p.one_line_desc,
+//            	p.place_lot_address,
+//            	p.place_road_address,
+//            	p.place_type,
+//            	p.like_count,
+//                p.view_count
+//            FROM PLACES p
+//            ORDER BY created_at DESC
+//           """)
+
+
     @Query("""
             select p
             from Places p
@@ -41,7 +57,8 @@ public interface PlacesRepository extends JpaRepository<Places, Long> {
               )
             order by p.createdAt desc
             """)
-    List<Places> searchPlacesExistsTags(
+    Page<Places> searchPlacesExistsTags(
+            @Param("pageable") Pageable pageable,
             @Param("q") String q,
             @Param("region") String region,
             @Param("type") String type,
