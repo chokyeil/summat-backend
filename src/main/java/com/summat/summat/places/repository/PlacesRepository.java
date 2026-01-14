a2821d8d66c687cd7cc199c0808bd02585995859
chokyeil <meme951023@gmail.com>
2026-01-14 18:03:20 +0900

    "feat: main list query modified"

--- src/main/java/com/summat/summat/places/repository/PlacesRepository.java
+++ src/main/java/com/summat/summat/places/repository/PlacesRepository.java
@@ -1,8 +1,10 @@
 package com.summat.summat.places.repository;
 
 import com.summat.summat.enums.PlaceTagType;
+import com.summat.summat.places.dto.places.response.PlacesFindResponseDto;
 import com.summat.summat.places.entity.Places;
 
+import com.summat.summat.places.repository.dto.PlacesFindResponseProjection;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -20,20 +22,23 @@ public interface PlacesRepository extends JpaRepository<Places, Long> {
     @Query("UPDATE Places p SET p.viewCount = p.viewCount + 1 WHERE p.id = :placeId")
     boolean increaseViews(@Param("placeId") Long placeId);
 
-//    @Query("""
-//            SELECT
-//            	p.places_id,
-//            	p.place_name,
-//            	p.place_image_url,
-//            	p.one_line_desc,
-//            	p.place_lot_address,
-//            	p.place_road_address,
-//            	p.place_type,
-//            	p.like_count,
-//                p.view_count
-//            FROM PLACES p
-//            ORDER BY created_at DESC
-//           """)
+    @Query(value = "SELECT " +
+                        "p.places_id            AS placesId, " +
+                        "p.place_name           AS placeName, " +
+                        "p.place_image_url      AS placeImageUrl, " +
+                        "p.one_line_desc        AS oneLineDesc, " +
+                        "p.place_lot_address    AS placeLotAddress, " +
+                        "p.place_road_address   AS placeRoadAddress, " +
+                        "p.place_type           AS placeType, " +
+                        "p.like_count           AS likeCount, " +
+                        "p.view_count           AS viewCount," +
+                        "p.created_at           AS createdAt " +
+                   "FROM places p " +
+                   "ORDER BY p.created_at DESC"
+            , countQuery = "SELECT count(*) " +
+                        "FROM places"
+            , nativeQuery = true)
+    Page<PlacesFindResponseProjection> findMainList(Pageable pageable);
 
 
     @Query("""
@@ -57,7 +62,7 @@ public interface PlacesRepository extends JpaRepository<Places, Long> {
               )
             order by p.createdAt desc
             """)
-    Page<Places> searchPlacesExistsTags(
+    Page<PlacesFindResponseDto> searchPlacesExistsTags(
             @Param("pageable") Pageable pageable,
             @Param("q") String q,
             @Param("region") String region,
