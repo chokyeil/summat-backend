600cb6fc5e7e471054b0d128ad28d026dc36128c
chokyeil <meme951023@gmail.com>
2026-01-14 18:04:58 +0900

    "feat: pakage move"

--- src/main/java/com/summat/summat/places/dto/places/response/PlaceListPageResDto.java
+++ src/main/java/com/summat/summat/places/dto/places/response/PlaceListPageResDto.java
@@ -1,9 +1,8 @@
-package com.summat.summat.places.dto.places;
+package com.summat.summat.places.dto.places.response;
 
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Getter
