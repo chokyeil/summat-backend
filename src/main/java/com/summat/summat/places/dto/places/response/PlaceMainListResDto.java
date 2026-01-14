13da8bf7b220490b2b7e9582f977dc391d660a32
chokyeil <meme951023@gmail.com>
2026-01-14 18:04:44 +0900

    "feat: pakage move, createdAt field add"

--- src/main/java/com/summat/summat/places/dto/places/response/PlaceMainListResDto.java
+++ src/main/java/com/summat/summat/places/dto/places/response/PlaceMainListResDto.java
@@ -1,10 +1,12 @@
-package com.summat.summat.places.dto.places;
+package com.summat.summat.places.dto.places.response;
 
 import com.summat.summat.enums.PlaceTagType;
+import com.summat.summat.places.repository.dto.PlacesFindResponseProjection;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.Instant;
 import java.util.List;
 
 @Getter
@@ -20,4 +22,9 @@ public class PlaceMainListResDto {
     private List<PlaceTagType> tags;
     private long likeCount;
     private long viewCount;
+    private Instant createdAt;
+
+//    private PlacesFindResponseDto placesFindResponseDto;
+//    private PlacesFindResponseProjection placesFindResponseProjection;
+//    private List<PlaceTagType> tags;
 }
