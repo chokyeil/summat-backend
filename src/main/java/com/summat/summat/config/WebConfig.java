e807a196d4d90c55446ede54700d746ddbaf97f4
chokyeil <meme951023@gmail.com>
2026-02-27 20:38:45 +0900

    "feat: "cors 설정"

--- src/main/java/com/summat/summat/config/WebConfig.java
+++ src/main/java/com/summat/summat/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.summat.summat.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,6 +19,15 @@ public class WebConfig implements WebMvcConfigurer {
         // 장소 이미지
         registry.addResourceHandler("/uploads/places/**")
                 .addResourceLocations("file:" + uploadDir + "/places/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
 
